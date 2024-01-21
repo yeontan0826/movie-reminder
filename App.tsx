0cef0d0bff26008e537f1b317778aa6ee2074302
류연찬 <yeontan0826@gmail.com>
2024-01-22 02:02:26 +0900

    react-query 사용을 위해 QueryClientProvider 추가

--- App.tsx
+++ App.tsx
@@ -1,15 +1,20 @@
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { NavigationContainer } from '@react-navigation/native';
+import { QueryClient, QueryClientProvider } from 'react-query';
 
 import RootStackNavigation from './src/navigations/rootStack';
 
+const queryClient = new QueryClient();
+
 const App = (): JSX.Element => {
   return (
-    <SafeAreaProvider>
-      <NavigationContainer>
-        <RootStackNavigation />
-      </NavigationContainer>
-    </SafeAreaProvider>
+    <QueryClientProvider client={queryClient}>
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <RootStackNavigation />
+        </NavigationContainer>
+      </SafeAreaProvider>
+    </QueryClientProvider>
   );
 };
 
