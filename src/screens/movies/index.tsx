5ab81913f035e1a75975f30674988a35e398fdf2
류연찬 <yeontan0826@gmail.com>
2024-01-22 02:03:00 +0900

    생성한 useMovies 적용

--- src/screens/movies/index.tsx
+++ src/screens/movies/index.tsx
@@ -1,4 +1,4 @@
-import { FlatList, StatusBar } from 'react-native';
+import { FlatList, RefreshControl, StatusBar } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import styled from 'styled-components/native';
 import Colors from 'open-color';
@@ -6,20 +6,33 @@ import Colors from 'open-color';
 import useMovies from '../../hooks/useMovies';
 import Movie from '../../components/movie';
 import Separator from '../../components/separator';
+import Loading from '../../components/loading';
 
 const MoviesScreen = (): JSX.Element => {
-  const { movies } = useMovies();
+  const { movies, isLoading, loadMore, refresh } = useMovies();
 
   return (
     <Container>
       <StatusBar barStyle={'light-content'} backgroundColor={Colors.black} />
-      <FlatList
-        contentContainerStyle={{ padding: 20 }}
-        indicatorStyle="white"
-        data={movies}
-        renderItem={({ item: movie }) => <Movie {...movie} />}
-        ItemSeparatorComponent={() => <Separator space={14} />}
-      />
+      {isLoading ? (
+        <Loading />
+      ) : (
+        <FlatList
+          contentContainerStyle={{ padding: 20 }}
+          indicatorStyle="white"
+          data={movies}
+          renderItem={({ item: movie }) => <Movie {...movie} />}
+          ItemSeparatorComponent={() => <Separator space={14} />}
+          onEndReached={loadMore}
+          refreshControl={
+            <RefreshControl
+              refreshing={isLoading}
+              onRefresh={refresh}
+              tintColor={Colors.white}
+            />
+          }
+        />
+      )}
     </Container>
   );
 };
